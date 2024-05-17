9cbd8457a86345bfe9f70e82876c33735bb59332
Doheera-kosi <kupourevans99@gmail.com>
2024-05-17 17:38:22 +0000

    Add API Endpoint to create a Restaurant

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -8,8 +8,8 @@ const handleValidationErrors = async (
 ) => {
   const errors = validationResult(req);
 
-  if(!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array})
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array });
   }
 
   next();
@@ -23,5 +23,31 @@ export const validateMyUserRequest = [
     .withMessage("AddressLine must be a string"),
   body("city").isString().notEmpty().withMessage("City must be a string"),
   body("country").isString().notEmpty().withMessage("Country must be a string"),
+  handleValidationErrors,
+];
+
+//
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+  body("city").notEmpty().withMessage("City name is required"),
+  body("country").notEmpty().withMessage("Country name is required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated delivery time must be a postive integer"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("meneItems.*.name").notEmpty().withMessage("Menu Item name is required"),
+  body("meneItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu Item price is required and must be positive number"),
+
   handleValidationErrors,
 ];
