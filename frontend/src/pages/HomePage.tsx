71626bfd91ad88dda7b40151de7f91fd3656cec7
Doheera-kosi <kupourevans99@gmail.com>
2024-05-19 12:13:35 +0000

    Fix searchQuery issue

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,40 +1,47 @@
 import landingImage from "../assets/landing.png";
-import appDownloadImage from "../assets/appDownload.png"
+import appDownloadImage from "../assets/appDownload.png";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const [searchQuery] = useState(""); 
 
   const handleSearchSubmit = (searchFormValues: SearchForm) => {
     navigate({
       pathname: `/search/${searchFormValues.searchQuery}`,
-    })
-  }
+    });
+  };
+
   return (
     <div className="flex flex-col gap-12">
-      <div className="md:px-60  bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
-        <h1 className="text-3xl font-bold tracking-tight text-orange-600">
-          Enjoy a delicious takeaway meal today!
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+          Tuck into a takeaway today
         </h1>
-        <span className="text-xl">Food is just a click away!!!</span>
-        <SearchBar placeHolder="Search by City or Town" onSubmit={handleSearchSubmit}/>
+        <span className="text-xl">Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+          searchQuery={searchQuery}
+        />
       </div>
-
       <div className="grid md:grid-cols-2 gap-5">
-        <img src={landingImage}/>
+        <img src={landingImage} />
         <div className="flex flex-col items-center justify-center gap-4 text-center">
           <span className="font-bold text-3xl tracking-tighter">
             Order takeaway even faster!
           </span>
           <span>
-            Download the Let&apos;sEat App for faster ordering and personalised recommendations
+            Download the MernEats App for faster ordering and personalised
+            recommendations
           </span>
           <img src={appDownloadImage} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
