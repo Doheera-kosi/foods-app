9f2954d7476bf29b5d462b5850c1f61d0af74b0a
Doheera-kosi <kupourevans99@gmail.com>
2024-05-29 12:56:10 +0000

    Add customer orders api

--- backend/src/routes/OrderRoute.ts
+++ backend/src/routes/OrderRoute.ts
@@ -4,6 +4,8 @@ import OrderController from "../controllers/OrderController";
 
 const router = express.Router();
 
+router.get("/", jwtCheck, jwtCheck, OrderController.getMyOrders);
+
 router.post(
   "/checkout/create-checkout-session",
   jwtCheck,
